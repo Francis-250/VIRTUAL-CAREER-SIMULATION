db52af4e5d66d6ede573c966aa45af680dc57938
Francis-250 <munyankindif0@gmail.com>
2026-09-18 09:11:30 +0200

    feat: database

--- config/database.php
+++ config/database.php
@@ -1,7 +1,12 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$host = 'localhost';
+$username = 'francois';
+$password = '123';
+$database = 'career_sim';
+
 try {
-    $con = new mysqli(getenv('DB_HOST') ?: '127.0.0.1', getenv('DB_USER') ?: 'root', getenv('DB_PASS') ?: '', getenv('DB_NAME') ?: 'career_sim');
+    $con = new mysqli($host, $username, $password, $database);
     $con->set_charset('utf8mb4');
 } catch (mysqli_sql_exception $e) {
     http_response_code(503);
